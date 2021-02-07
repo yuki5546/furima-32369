ee0056fcdc40743506d49db70c4d8cdd54f57a0b
yuki5546 <inou0531@gmail.com>
2021-02-07 11:22:16 +0900

    ユーザー管理機能の実装#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
-  validates :nickname, presence: true
+  with_options presence: true do
+  validates :nickname
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } # ユーザー本名は全角 （漢字・ひらがな・カタカナ）
-  validates :last_name_kana, :first_name_kana, presence: true,
-                                               format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true
-  validates :user_birth_date, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } # ユーザー本名は全角 （漢字・ひらがな・カタカナ）
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' }
+  validates :user_birth_date
+  end
 
   has_many :items, dependent: :destroy
   has_many :transaction_items
