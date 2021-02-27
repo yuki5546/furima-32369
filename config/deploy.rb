d7a577ed388467907239f401dc7f7f82cad36c29
yuki5546 <inou0531@gmail.com>
2021-02-27 17:27:53 +0900

    自動デプロイの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'furima-32369'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com/yuki5546/furima-32369.git'
+set :repo_url,  'git@github.com:yuki5546/furima-32369.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
