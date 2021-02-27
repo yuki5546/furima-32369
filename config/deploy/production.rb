f4eee28452e3431033e25008e27b3274b46f2564
yuki5546 <inou0531@gmail.com>
2021-02-27 16:52:42 +0900

    自動デプロイ 修正#2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '18.180.212.250', user: 'ec2-user', roles: %w{app db web}', user: 'ec2-user', roles: %w{app db web}
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  server '18.180.212.250', user: 'ec2-user', roles: %w{app db web}', user: 'ec2-user', roles: %w{app db web}
-end
+
